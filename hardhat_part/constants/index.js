920aecd67376e31fdd1674e4fae62221e0566859
unknown <praveenpatidar171@gmail.com>
2023-04-28 16:02:41 +0530

    Uploded project

---
+++ hardhat_part/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xb8F26751A12a2987DbEEE1288C8254E576fb41A4";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
